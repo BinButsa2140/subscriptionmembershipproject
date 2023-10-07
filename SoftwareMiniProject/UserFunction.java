31e7ea3a6aa6e17db9cef718826266e16dd0d965
BinButsa2140 <142311704+BinButsa2140@users.noreply.github.com>
2023-10-07 16:12:44 +0700

    Delete SoftwareMiniProject directory
    need to update

--- SoftwareMiniProject/UserFunction.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDate;
-
-interface UserFunction {// ยังไม่ได้ใช้
-    public abstract String CheckStatus(boolean status);
-    public abstract Boolean CountDown(LocalDate date, LocalDate expire);
-}
